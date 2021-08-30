b08db8ce590121c142195f4d58c60de1a19afeee
Nachiappan <nachiapan@gmail.com>
2021-08-30 10:31:57 +0530

    Getting started with type script

---
+++ tsdemo/welcome1.js
@@ -0,0 +1,7 @@
+function greet(name) {
+    if (name === void 0) { name = 'nachi'; }
+    console.log('Hello ' + name);
+    console.log("Hello " + name);
+}
+greet();
+greet('name');
---
+++ tsdemo/welcome1.ts
@@ -0,0 +1,8 @@
+function greet(name = 'nachi'){
+    console.log('Hello ' +  name);
+    console.log(`Hello ${name}`);
+}
+
+
+greet();
+greet('my name');
