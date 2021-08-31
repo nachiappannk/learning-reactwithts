9a7825efedf28cab28bb636cb5f7303e105cadb7
Nachiappan <nachiapan@gmail.com>
2021-08-31 11:39:34 +0530

    Completed components with state

--- myapp/src/components/GuestList.tsx
+++ myapp/src/components/GuestList.tsx
@@ -2,12 +2,28 @@ import React, {useState} from "react";
 
 const GuestList : React.FC = () => {
 
-    const [names, setNames] = useState(''); 
+    const [name, setName] = useState('');
+    const [guests, setGuests] = useState<string[]>([]);
+    
+    const onClickButton = () =>{
+        var newGuests = [...guests, name];
+        setName('');
+        console.log(name);
+        setGuests(newGuests);
+
+        console.log(guests);
+    }
+
     return (
         <div>
-            <input type='text' value={names} onChange={(e) => setNames(e.target.value)}></input>
+            <input type='text' value={name} onChange={(e) => setName(e.target.value)}></input>
             <div>test guest list</div>
-            <button >Add Guest</button>
+            <button onClick={onClickButton}>Add Guest</button>
+            <ul>
+                {guests.map((x) => {
+                    return <li key={x}>{x}</li>
+                })}
+            </ul>
         </div>
     );
 };
