eeefc072255358cd0bfd0febda61371afc603381
Nachiappan <nachiapan@gmail.com>
2021-08-31 11:47:52 +0530

    Excercise done

--- myapp/src/components/GuestList.tsx
+++ myapp/src/components/GuestList.tsx
@@ -1,13 +1,21 @@
 import React, {useState} from "react";
 
+interface StateUnit {
+    name: string;
+    city: string;
+}
+
+
 const GuestList : React.FC = () => {
 
     const [name, setName] = useState('');
-    const [guests, setGuests] = useState<string[]>([]);
+    const [city, setCity] = useState('');
+    const [guests, setGuests] = useState<StateUnit[]>([]);
     
     const onClickButton = () =>{
-        var newGuests = [...guests, name];
+        var newGuests = [...guests, {"name": name, "city": city }];
         setName('');
+        setCity('');
         console.log(name);
         setGuests(newGuests);
 
@@ -17,11 +25,12 @@ const GuestList : React.FC = () => {
     return (
         <div>
             <input type='text' value={name} onChange={(e) => setName(e.target.value)}></input>
+            <input type='text' value={city} onChange={(e) => setCity(e.target.value)}></input>
             <div>test guest list</div>
             <button onClick={onClickButton}>Add Guest</button>
             <ul>
                 {guests.map((x) => {
-                    return <li key={x}>{x}</li>
+                    return <li key={x.name}>{x.name}-{x.city}</li>
                 })}
             </ul>
         </div>
