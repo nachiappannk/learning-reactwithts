130a38b32370cbbad26129e5ccddd656a1095fbe
Nachiappan <nachiapan@gmail.com>
2021-09-02 10:38:55 +0530

    Complete the landing page

--- myapp/src/components/LandingPage.tsx
+++ myapp/src/components/LandingPage.tsx
@@ -1,20 +1,26 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
 import Login from './Login';
 import PageNotFound from "./PageNotFound";
 import Register from "./Register";
 
-
+const Header = () =>{
+    return (<header>
+        <NavLink to="login">Login</NavLink>
+        <NavLink to="register">Register</NavLink>
+    </header>);
+}
  
 const LandingPage=()=>{
     return (
     <div>
         Dummy testzz
         <BrowserRouter>
+            <Header/>
             <Switch>
-                <Route path='/' component={Register}/>
                 <Route path='/login' component={Login}/> 
                 <Route path='/register' component={Register}/>
+                <Route path='/' component={Register} exact={true}/>
                 <Route component={PageNotFound}/>
             </Switch>
         </BrowserRouter>
