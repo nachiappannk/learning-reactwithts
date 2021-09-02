576a817899e503f79bc192dcf97787f2f326d8ec
Nachiappan <nachiapan@gmail.com>
2021-09-02 10:20:58 +0530

    Added register route

--- myapp/src/components/LandingPage.tsx
+++ myapp/src/components/LandingPage.tsx
@@ -1,27 +1,22 @@
 import React from "react";
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './Login';
+import Register from "./Register";
 
 
  
 const LandingPage=()=>{
     return (
     <div>
-        Records loads here....
-        {routes}
+        Dummy testzz
+        <BrowserRouter>
+            <div>
+                <Route path='/login' component={Login}/> 
+                <Route path='/register' component={Register}/> 
+            </div>
+        </BrowserRouter>
         </div>
     )
 }
 
 export default LandingPage
-
-
-
-const routes =(
-    <BrowserRouter>
-    <div>
-        <Route path='/login' component={Login}/> 
-    </div>
-    </BrowserRouter>
-
-)
