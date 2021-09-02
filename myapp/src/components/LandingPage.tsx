e26a6aed2d4c81f692966cede2a376ac2c967666
Nachiappan <nachiapan@gmail.com>
2021-09-02 10:28:14 +0530

    Fixed the basic routing to default

--- myapp/src/components/LandingPage.tsx
+++ myapp/src/components/LandingPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './Login';
+import PageNotFound from "./PageNotFound";
 import Register from "./Register";
 
 
@@ -12,7 +13,9 @@ const LandingPage=()=>{
         <BrowserRouter>
             <div>
                 <Route path='/login' component={Login}/> 
-                <Route path='/register' component={Register}/> 
+                <Route path='/register' component={Register}/>
+                <Route component={PageNotFound}/>
+                
             </div>
         </BrowserRouter>
         </div>
